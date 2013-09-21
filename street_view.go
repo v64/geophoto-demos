2bfe383baa1837a4b7c0c1aca3cb24144a388f33
Jahn Veach <j@hnvea.ch>
2013-09-21 06:47:50 -0500

    initial commit

---
+++ street_view.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/v64/geophoto"
+	"io"
+	"net/http"
+	"os"
+	"sort"
+	"strings"
+	"time"
+)
+
+func main() {
+	flag.Parse()
+
+	inDir := flag.Arg(0)
+	if inDir == "" {
+		fmt.Fprintf(os.Stderr, "Input directory is required\n")
+		os.Exit(1)
+	}
+
+	outDir := flag.Arg(1)
+	if outDir == "" {
+		fmt.Fprintf(os.Stderr, "Output directory is required\n")
+		os.Exit(1)
+	}
+
+	if !strings.HasSuffix(outDir, "/") {
+		outDir += "/"
+	}
+
+	_, err := os.Stat(outDir)
+	if err == nil {
+		fmt.Fprintf(os.Stderr, "Output directory already exists\n")
+		os.Exit(1)
+	}
+
+	err = os.MkdirAll(outDir, 0755)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error creating output directory: %s\n", err.Error())
+		os.Exit(1)
+	}
+
+	geoDatas := geophoto.DirGeoPhotoData(inDir)
+
+	var timestamps []int
+	for timestamp, _ := range geoDatas {
+		timestamps = append(timestamps, timestamp)
+	}
+
+	sort.Ints(timestamps)
+	total := len(timestamps)
+
+	for i, timestamp := range timestamps {
+		geo := geoDatas[timestamp]
+		fmt.Println("Downloading", i+1, "of", total)
+		getStreetViewImage(outDir, fmt.Sprintf("%05d", i+1), geo.StreetViewUrl())
+	}
+}
+
+func getStreetViewImage(path, name, url string) {
+	fileName := path + name + ".jpg"
+
+	out, err := os.Create(fileName)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error creating file %s: %s\n", fileName, err.Error())
+		return
+	}
+	defer out.Close()
+
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error getting URL %s: %s\n", url, err.Error())
+		return
+	}
+	defer resp.Body.Close()
+
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error writing file %s: %s\n", url, err.Error())
+		return
+	}
+
+	// Avoid hitting google's rate limit
+	time.Sleep(time.Second)
+}
